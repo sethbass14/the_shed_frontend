1f8ab009ed5727acd6ba8e65ef6faef073cf7951
github username <github email address>
2018-02-05 13:28:04 -0500

    got the refresh working on the routes

--- src/components/SongShow.js
+++ src/components/SongShow.js
@@ -10,12 +10,14 @@ const SongShow = props => {
   return (
     <div>
       <div>
-        {props.song? (
-          <h2>{props.song.title}</h2>
+        {props.song.id? (
+          <div>
+            <h2>{props.song.title}</h2>
+            <AudioPlayer song={props.song}/>
+          </div>
         ) : (
           null
         )}
-        <AudioPlayer song={props.song}/>
       </div>
     </div>
   )
@@ -23,9 +25,16 @@ const SongShow = props => {
 
 //Check out this problem of refreshing not working down below
 const mapStateToProps = (state, prevProps) => {
-  return {
-    band: state.userData.bands.find(band => band.id === parseInt(prevProps.match.params.bandId)),
-    song: state.userData.songs.find(song => song.id === parseInt(prevProps.match.params.songId))
+  if (state.userData.id) {
+    return {
+      band: state.userData.bands.find(band => band.id === parseInt(prevProps.match.params.bandId)),
+      song: state.userData.songs.find(song => song.id === parseInt(prevProps.match.params.songId))
+    }
+  } else {
+    return {
+      band: {},
+      song: {}
+    }
   }
 }
 
