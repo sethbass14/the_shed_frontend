d1cd40b6868cebd1105e0e48e50f02a57e20e26d
github username <github email address>
2018-02-08 10:48:08 -0500

    songs are rendered on the set list page

--- src/components/SetList/SetListShow.js
+++ src/components/SetList/SetListShow.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux';
+import SongListItem from '../Song/SongListItem'
+import * as actions from '../../actions';
 
 //Make this a class to add a search bar functionality in the future
 class SetListShow extends React.Component {
@@ -8,12 +12,45 @@ class SetListShow extends React.Component {
   }
 
   render() {
+    console.log('In Set List Show', this.props)
+    const bandSongs = this.props.bandSongs.map((song, index) =>  <SongListItem key={index} song={song}/>)
     return (
-      <div>
-        <h1>In the SetListShow</h1>
+      <div className="ui grid container">
+        <div className="sixteen wide column">
+          <h1>{this.props.band.id? `${this.props.band.name} Set: ${this.props.setList.name} ${this.props.setList.date}` : null}</h1>
+        </div>
+        <div className="four wide column">
+          <h2>Repertoire</h2>
+          {bandSongs}
+        </div>
+        <div className="eight wide column">
+          <h2>Song Cards</h2>
+        </div>
+        <div className="four wide column">
+          <h2>Ability to add a new song to the set list</h2>
+        </div>
       </div>
     )
   }
 }
 
-export default SetListShow
+const mapStateToProps = (state, ownProps) => {
+  const band = state.userData.bands.find(band => band.id === parseInt(ownProps.match.params.bandId))
+  if (band) {
+    const setList = band.set_lists.find(setList => setList.id === parseInt(ownProps.match.params.setListId))
+    return {
+      band,
+      bandSongs: state.userData.songs.filter(song => song.band_id === band.id),
+      setList
+    }
+  } else {
+      return {
+        band: {},
+        bandSongs: [],
+        setList: {}
+      }
+  }
+}
+
+
+export default withRouter(connect(mapStateToProps, actions)(SetListShow))
