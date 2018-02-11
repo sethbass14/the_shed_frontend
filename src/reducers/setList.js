3a61bd9bb31f24affa12521c8945a575aef4cbe2
github username <github email address>
2018-02-11 14:39:02 -0500

    added an ordered set list at creation

--- src/reducers/setList.js
+++ src/reducers/setList.js
@@ -1,4 +1,4 @@
-import { SET_USER_DATA, ADD_SET_LIST, DELETE_SET_LIST, DELETE_BAND, ADD_SET_SONG } from '../constants'
+import { SET_USER_DATA, ADD_SET_LIST, DELETE_SET_LIST, DELETE_BAND, ADD_SET_SONG, DELETE_SET_SONG } from '../constants'
 
 const initialState = []
 export const setListReducer = ( state = initialState, action) => {
@@ -12,10 +12,22 @@ export const setListReducer = ( state = initialState, action) => {
     case DELETE_SET_LIST:
       return state.filter(setList => setList.id !== action.setList.id)
     case ADD_SET_SONG:
+    case DELETE_SET_SONG:
       let setList = state.find(setList => setList.id === action.setSong.set_list_id)
       let index = state.indexOf(setList)
-      setList.song_ids = [...setList.song_ids, action.setSong.song_id]
+      setList.song_ids = setListSongIdReducer(setList.song_ids, action)
       return [...state.slice(0, index), setList, ...state.slice(index + 1)]
+    default:
+      return state
+  }
+}
+
+const setListSongIdReducer = (state = [], action) => {
+  switch(action.type) {
+    case ADD_SET_SONG:
+      return [...state, action.setSong.song_id]
+    case DELETE_SET_SONG:
+      return state.filter(songId => songId !== action.setSong.song_id)
     default:
       return state
   }
