3d110dda84aa4c684a8a51ab11bad53434c1bbcd
github username <github email address>
2018-02-11 14:48:57 -0500

    got the correct creation order happening

--- src/components/Song/SongListItem.js
+++ src/components/Song/SongListItem.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions'
 
 const SongListItem = props => {
-  // console.log('In Song List Item', props.setList.song_ids.length + 1)
+  // console.log('In Song List Item', props.setList.song_ids)
   return (
     <div>
       <Link to={`/bands/${props.song.band_id}/songs/${props.song.id}`}>
