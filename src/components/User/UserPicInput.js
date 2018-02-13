d70231845c07944dd9de1a3cee29c597e9918e14
github username <github email address>
2018-02-12 20:04:34 -0500

    got the form getting the file on the front end

--- src/components/User/UserPicInput.js
+++ src/components/User/UserPicInput.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 
 
 class UserPicInput extends React.Component {
+  constructor() {
+    super()
+  }
 
-handleFileSubmit = event => {
-  event.preventDefault()
-  console.log('in handleFileSubmit')
-}
+  handleFileSubmit = (event) => {
+    event.preventDefault()
+    console.log(event)
+    const image_upload = document.getElementById('image_upload')
+    const file = image_upload.files[0]
+    const formData = new FormData()
+    if (file) {
+      formData.append("avatar", file)
+      formData.append("id", this.props.user.id)
+      debugger
+    } else {
+      alert('Must upload a file on submit!')
+    }
+    console.log('in handleFileSubmit')
+  }
+
   render() {
     return (
       <div>
-        <form onSubmit={() => this.handleFileSubmit()}>
+        <form onSubmit={this.handleFileSubmit}>
           <input type="file" name="image" accept="image/*" id="image_upload"></input>
           <button className="ui button" type="submit">Submit</button>
         </form>
@@ -19,4 +36,10 @@ handleFileSubmit = event => {
   }
 }
 
-export default UserPicInput
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(UserPicInput)
