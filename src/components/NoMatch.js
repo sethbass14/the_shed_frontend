7d98791ff4572bf6fe2528dfd612205b8241d1fe
github username <github email address>
2018-04-09 15:50:03 -0400

    added link to dashboard on No Match

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NoMatch = props =>
   <div className='main-container'>
     <h1>404 Resource Not Found</h1>
+    <Link to="/dashboard">
+      Link to Dashboard
+    </Link>
   </div>
 
 export default NoMatch
